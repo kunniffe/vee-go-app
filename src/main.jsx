d4214850ca18bd0e6d2297ef5847c907a8e4f1aa
Jonathan Cunniffe <jonathan@jjmcunniffe.com>
2022-12-08 15:35:20 +0000

    Added MUI theming

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // MUI.
-import { CssBaseline } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import theme from "./services/theme";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CssBaseline>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </CssBaseline>
   </React.StrictMode>
 )
---
+++ src/services/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+// Temp.
+import { green } from "@mui/material/colors";
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: green[500],
+      }
+    },
+});
+  
+export default theme;
