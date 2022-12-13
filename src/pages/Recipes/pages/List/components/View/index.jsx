cd97dfbb805e77d7e0a8d3cc336dbc5cc81c9e15
Jonathan Cunniffe <jonathan@jjmcunniffe.com>
2022-12-13 16:34:22 +0000

    Added filtering to recipe list

--- src/pages/Recipes/pages/List/components/View/index.jsx
+++ src/pages/Recipes/pages/List/components/View/index.jsx
@@ -1,22 +1,47 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./index.css";
 
 const View = ({ data }) => {
+  const [recipes, setRecipes] = useState(data);
+
+  function handleFilterChange(event) {
+    const option = event.target.value;
+
+    // Filter only the recipes with option.
+    if (option === "filter") {
+      setRecipes(data);
+    } else {
+      const results = data.filter((recipe) => recipe.course === option);
+      setRecipes(results);
+    }
+  }
+
   return (
-    <ul className="Recipes__list">
-      {data.map((recipe, index) => (
-        <li key={index} className="Recipes__list__listItem">
-          <Link to={`/recipes/${recipe.id}`}>
-            <h3 className="Recipes__list__listItem__name">{recipe.name}</h3>
-            <img src={`/images/${recipe.image}`} alt={recipe.name} className="Recipes__list__listItem__image" />
-            <br />
-            <p className="Recipes__list__listItem__desc">{recipe.description}</p>
-            <br />
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <select 
+        name="Recipes__filter"
+        className="Recipes__filter"
+        onChange={handleFilterChange}
+      >
+        {["Filter", "Breakfast", "Lunch", "Dinner", "Dessert"].map((meal, index) => (
+          <option key={index} value={meal.toLowerCase()}>{meal}</option>
+        ))}
+      </select>
+
+      <ul className="Recipes__list">
+        {recipes.map((recipe, index) => (
+          <li key={index} className="Recipes__list__listItem">
+            <Link to={`/recipes/${recipe.id}`}>
+              <h3 className="Recipes__list__listItem__name">{recipe.name}</h3>
+              <img src={`/images/${recipe.image}`} alt={recipe.name} className="Recipes__list__listItem__image" />
+              <p className="Recipes__list__listItem__desc">{recipe.description}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
