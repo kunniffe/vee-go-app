9fe0ca7de9412a09e82b08c9ee9ff8164f65f181
Jonathan Cunniffe <kunniffe@gmail.com>
2022-12-14 11:29:36 +0000

    Added homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet";
 
 import Layout from "../../components/Layout";
 import Page from "../../components/Page";
+import "./index.css";
 
 function HomePage() {
   return (
@@ -10,7 +11,18 @@ function HomePage() {
         <title>Home | Veego</title>
       </Helmet>
       <Page title="Home">
-        <p>Hello, Home!</p>
+        <h1> Welcome to Veego</h1>
+        <p>
+          Welcome to the Veego Website! Here you can find lovely vegan receipes
+          and even get the ingredients delivered straight to your door if you
+          want. If you want to dine at a vegan resturant, you can use the
+          location function, in order to find a vegan resturant near you.
+        </p>
+        <img
+          className="Home__image"
+          src="./images/homepage.jpg"
+          alt="homepage"
+        />
       </Page>
     </Layout>
   );
